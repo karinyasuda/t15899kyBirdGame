4da0e5e5890c873b58901d4cc1fdb78cb3820a59
Karin <karin.03k35@gmail.com>
2016-07-04 12:44:20 +0900

    no message

---
+++ Assets/WallScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallScript : MonoBehaviour {
+	private float speed = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		transform.position += transform.forward * speed * Time.deltaTime;
+		transform.Translate(-0.1f , 0, 0);	
+	}
+	void OnCollisionEnter() {
+		Debug.Log ("がベガぶつかった");
+		Time.timeScale = 0;
+
+	}
+}
