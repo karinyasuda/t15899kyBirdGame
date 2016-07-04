eaa24b614c2d700cbbf1e42a9b1718902e0f5bb5
Karin <karin.03k35@gmail.com>
2016-07-04 12:43:19 +0900

    test

---
+++ Assets/WallCreaterScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCreaterScript : MonoBehaviour {
+	public GameObject Wall;
+	float timer = 0.3f;
+	int interval = 3;
+	int number = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (number == 70) {
+			Vector3 pos = transform.position;
+			pos.y = UnityEngine.Random.Range (0, 4);
+			transform.position = pos;
+			Instantiate (Wall, transform.position, Quaternion.identity);
+			number = 0;
+		}
+		number++;
+			
+	}
+}
