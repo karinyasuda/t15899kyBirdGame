ab6bffcf4b8f5ee396a1846dd558a3e47447e1e7
Karin <karin.03k35@gmail.com>
2016-07-04 12:42:46 +0900

    first commit  ゲームの形はできました！！

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class PlayerScript : MonoBehaviour {
+	public GameObject text;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)){
+			GetComponent<Rigidbody> ().velocity = new Vector3 (0,6,0);
+
+		}
+	
+	}
+	void OnCollisionEnter() {
+		text.SetActive (true);
+		text.GetComponent<TextMesh>().text ="GameOver! \n スコア" + (int)Time.realtimeSinceStartup+ "点でした";
+
+
+	}
+}
